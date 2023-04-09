9322b998d587a19674cc98d84c78dcabb3c8e60c
Alen Boric <boricalen@live.com>
2023-04-10 00:58:49 +0200

    Login Form

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,8 +10,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.all(15),
+            child: Image.asset(
+              "images/seller.png",
+              height: 270,
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObsecure: false,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObsecure: true,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30,),
+          ElevatedButton(
+            onPressed: () => print("clicked"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              padding:
+              const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),),
+            child: const Text(
+              "Login",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
