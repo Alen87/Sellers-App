1d06f0df4d04721585512e2eee274afb2be458e5
Alen Boric <boricalen@live.com>
2023-04-09 20:06:42 +0200

    Custom Text Form Field

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sellers_app/widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -8,8 +9,10 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController anyController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
